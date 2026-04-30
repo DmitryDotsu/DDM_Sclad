bc6822e2bb8c11a7ca8ed153c900421bd328db51
DmitryDotsu <dotsu_dmitry@mail.ru>
2026-04-30 19:10:38 +0500

    Update 02_Index.sql
    fix 
    Убираю обязательность заполнения некоторых полей

--- sql/02_Index.sql
+++ sql/02_Index.sql
@@ -21,3 +21,12 @@ CREATE INDEX idx_персонал_фио ON персонал(фио);
 CREATE INDEX idx_контрагенты_название ON контрагенты(название);
 CREATE INDEX idx_товары_название ON товары(название);
 CREATE INDEX idx_товары_категория ON товары(категория);
+
+--30.04.2026 FIX - мешает реализации
+-- 1. Разрешаем NULL для телефона, адреса, email
+ALTER TABLE контрагенты ALTER COLUMN телефон DROP NOT NULL;
+ALTER TABLE контрагенты ALTER COLUMN адрес DROP NOT NULL;
+ALTER TABLE контрагенты ALTER COLUMN email DROP NOT NULL;
+
+-- 2. Удаляем ограничение CHECK, которое требует, чтобы была хотя бы одна галочка
+ALTER TABLE контрагенты DROP CONSTRAINT IF EXISTS контрагенты_check;
